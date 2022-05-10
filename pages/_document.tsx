db39c4b93fd3f48d950ebd37ce9461e0bc3266e8
arshptl <arshpatel714@gmail.com>
2022-05-10 20:56:39 +0530

    added dependences

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Head, Main, NextScript, Html } from 'next/document'
 import { extractStyles } from 'evergreen-ui'
 
 export default class MyDocument extends Document<any> {
-  static getInitialProps({ renderPage }: any) {
+  static getInitialProps({ renderPage }) {
     const page = renderPage()
     const { css, hydrationScript } = extractStyles()
 
